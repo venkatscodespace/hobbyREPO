37a73eba4e540580b3ff90d335ccbec503a655d7
venkatcodesstuff <venkatram.csc1@gmail.com>
2024-06-14 21:40:46 +0530

    Create python-arithmetic-operators-hackerrank.py

---
+++ python-programming/python-arithmetic-operators-hackerrank.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    powr=10**10
+    a = int(input())
+    b = int(input())
+    if (a in range(1,powr)) and (b in range(1,powr)):
+        print(a+b)
+        print(a-b)
+        print(a*b)
