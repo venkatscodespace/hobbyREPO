dbc29bb090c1b29bb639fc443a21197f3c051953
Venkatram <venkatram.csc1@gmail.com>
2024-06-27 09:57:06 +0530

    update open-link-on-time.py

--- python-programming/open-link-on-time.py
+++ python-programming/open-link-on-time.py
@@ -1,9 +1,15 @@
-:from webbrowser import open_new
 from datetime import datetime
+from webbrowser import open_new
+from datetime import datetime
+import time
 def get_current_time_hh_mm():
     now = datetime.now()
     return now.strftime("%H:%M")
-time=get_current_time_hh_mm()
-while time=='09:28':
-    open_new('https://netacad.webex.com/netacad/j.php?MTID=mfa0f4792b3511fc25bc0ff4d98403231')
-    time=get_current_time_hh_mm()
+target_time = '09:56'
+opened = False
+while not opened:
+    current_time = get_current_time_hh_mm()
+    if current_time == target_time:
+        open_new('https://netacad.webex.com/netacad/j.php?MTID=mfa0f4792b3511fc25bc0ff4d98403231')
+        opened = True
+    time.sleep(30)  # Check every 30 seconds
