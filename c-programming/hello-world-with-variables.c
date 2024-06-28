de6fa7a1bc40fc0dbd505c8cee53d3975201e9c9
Venkatram <venkatram.csc1@gmail.com>
2024-06-28 10:06:45 +0530

    create hello-world-with-variables-and-multipleprintstatements.c

---
+++ c-programming/hello-world-with-variables.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void)
+{
+    char name[]="Venkat";
+    char platform[]="VS Code";
+    printf("Hello, %s! \n",name);
+    printf("This is C Programming in %s!",platform);
+}
