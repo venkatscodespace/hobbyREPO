7110553bd9515c1e1ee34503ebc34a0d6f76100b
Venkatram <venkatram.csc1@gmail.com>
2024-06-28 10:15:27 +0530

    create addition.c

--- c-programming/hello-world-with-variables.c
+++ c-programming/hello-world-with-variables.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(void)
 {
-    char name[]="Venkat";
+    int name[]="Venkat";
     char platform[]="VS Code";
     printf("Hello, %s! \n",name);
     printf("This is C Programming in %s!",platform);
