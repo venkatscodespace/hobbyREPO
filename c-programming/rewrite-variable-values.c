1187158e1bd280abb4b4ad1ab969623cf3496630
Venkatram <venkatram.csc1@gmail.com>
2024-07-01 14:16:07 +0530

    added messages by orintf to indicate change of value

--- c-programming/rewrite-variable-values.c
+++ c-programming/rewrite-variable-values.c
@@ -3,6 +3,7 @@ int main() {
     printf("Enter Number: ");
     int n;
     scanf("%d",&n);
+    printf("Before Updation of value: %d",n);
     n=n*4; //there is no need to declare the variable again as int 
-    printf("%d",n);
+    printf("After Updation of value: %d",n);
 }
