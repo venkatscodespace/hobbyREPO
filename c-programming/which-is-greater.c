e1504772a5df1a9e686b046bfaac04b48d7979c6
Venkatram <venkatram.csc1@gmail.com>
2024-07-16 19:02:37 +0530

    Add files via upload

---
+++ c-programming/which-is-greater.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+    printf("Enter Number 1: ");
+    int n1;
+    scanf("%i",&n1);
+    printf("Enter Number 2: ");
+    int n2;
+    scanf("%i",&n2);
+    if (n1>n2){
+        printf("%d is greater than %d.\n",n1,n2);
+    }
+    else if (n1<n2){
+        printf("%d is greater than %d.\n",n2,n1);
+    }
+    else {
+        printf("Both numbers are equal.\n");
+    }
+}
